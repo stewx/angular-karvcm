39f8b7d74f626a1f137a950a7ec1b707419526fc
Stewart Rand <stewart.rand@quest.com>
2021-05-03 13:49:20 -0300

    init

---
+++ src/app/services/to-do.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+
+@Injectable()
+export class NavigationService {
+
+  constructor() {
+
+  }
+
+  logChange() {
+    // TODO: log to the following URL, and then log to console if it succeeded or failed
+    // URL: https://jsonplaceholder.typicode.com/posts
+
+
+  }
+
+}
